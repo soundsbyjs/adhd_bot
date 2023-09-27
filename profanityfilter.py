14e566c5c301fbabf5cd1247ea231185d284f515
soundsbyjs <johnscott.schuette7@gmail.com>
2023-09-27 07:19:02 -0400

    Updated profanity filter

--- profanityfilter.py
+++ profanityfilter.py
@@ -1,48 +1,39 @@
 # read in an array of strings, for loop it, replace with 
 # family friendly language lnao
 # uses many tik tok conventions such as "unalive" for "kill"
-def censor(words):
-    for word in words:
-        if word == "fuck": 
-            word = "f"
-        if word == "fucking": 
-            word = "fucked"
-        if word == "fucked": 
-            word = "effed" 
-        if word == "shit": 
-            word = "poop"
-        if word == "shitting": 
-            word = "pooping"
-        # as in, couldn't give two shits!
-        # as you can tell this is a *lot* of anticipation
-        if word == "shits": 
-            word = "craps"
-        if word == "bitch": 
-            word = "b-word"
-        if word == "bitches": 
-            word = "b-words"
-        if word == "bitching": 
-            word = "b-wording"
-        if word == "cunt":
-            word = "see you next tuesday"
-        if word == "cock":
-            word = "weiner"
-        if word == "cocks":
-            word = "weiner"
-            # "a fallatious woman... er, person!"
-        if word == "cocksucker":
-            word = "chickenhead"
-        if word == "motherfucker":
-            word = "mf"
-        if word == "tit":
-            word = "breast"
-        if word == "tits":
-            word = "breasts"
-        if word == "":
-            word = "see you next tuesday"
-        if word == "kill": 
-            word = "unalive"
-        if word == "kys": 
-            word = "unalive yourself"
 
-        # what do I do about racial slurs?
+def censor(words):
+    profanity_dict = {
+        "fuck": "f",
+        "fucking": "fucked",
+        "fucked": "effed",
+        "shit": "poop",
+        "shitting": "pooping",
+        "shits": "craps",
+        "bitch": "b-word",
+        "bitches": "b-words",
+        "bitching": "b-wording",
+        "cunt": "see you next tuesday",
+        "cock": "weiner",
+        "cocks": "weiners",
+        "cocksucker": "chickenhead",
+        "motherfuck": "mf",
+        "motherfucker": "mf",
+        "tit": "breast",
+        "tits": "breasts",
+        "cunt": "see you next tuesday",
+        "kill": "unalive",
+        "kys": "unalive yourself",
+        "sex": "s-eggs",
+        # Add more words as needed
+    }
+
+    censored_words = []
+    for word in words:
+        lower_word = word.lower()
+        if lower_word in profanity_dict:
+            censored_words.append(profanity_dict[lower_word])
+        else:
+            censored_words.append(word)
+
+    return censored_words
